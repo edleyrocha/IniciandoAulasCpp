33b0a69b04baee96af25cac5f3220f2c88ae7a40
Edley Allan Hoyler <edleyrocha@hotmail.com>
2015-11-26 17:58:15 -0300

    Aula4

---
+++ AulasIniciantesCpp/Aula4/Aula4.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+
+	// Comentario Tipo 1 - Linha Toda
+	/* Comentario Tipo 2 - Selecionado */
+
+	int numeroA = (3), numeroB = (6);
+	int numeroC = (7);
+	int numeroD, numeroE;
+	numeroD = numeroE = (2);
+
+	printf(("Valor Numero A ........: %d \n\n"), (numeroA));
+	printf(("Valor Numero B ........: %d \n\n"), (numeroB));
+	printf(("Valor Numero C ........: %d \n\n"), (numeroC));
+	printf(("Valor Numero D ........: %d \n\n"), (numeroD));
+	printf(("Valor Numero E ........: %d \n\n"), (numeroE));
+
+	system("PAUSE");
+	return (0);
+};
